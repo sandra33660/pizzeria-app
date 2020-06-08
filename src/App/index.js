6689ccdffa41e2bd16d84f3fa80da7ec88596a28
sandra <60742092+sandra33660@users.noreply.github.com>
2020-06-08 18:07:47 +0200

    feat: 🎸 use react-query to fetch/pizzas

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,32 @@
 import React from "react";
+
 import PizzaList from "../PizzaList";
-import { CssBaseline } from "@material-ui/core";
+import { CircularProgress, CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { useQuery } from "react-query";
 
 import { theme } from "./theme";
 import Header from "../Header";
-import db from "../db.json";
+
+const fetchPizzas = () =>
+  fetch("http://localhost:3001/pizzas").then((response) => response.json());
+
 export default function App() {
-  const pizzas = db.pizzas;
+  /*  const [pizzas, setPizzas] = React.useState([]); */
+  const { status, data } = useQuery("pizzas", fetchPizzas);
+
+  /*  React.useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+      .then((response) => response.json())
+      .then((pizzas) => setPizzas(pizzas));
+  }, []); */
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header shoppingCartCount={3} />
-      <PizzaList data={pizzas} />
+      {status === "loading" && <CircularProgress />}
+      {status === "success" && <PizzaList data={data} />}
     </ThemeProvider>
   );
 }
