26bb2020deff02f0cbbd93a9b6d26091db720737
sandra <60742092+sandra33660@users.noreply.github.com>
2020-06-09 11:15:05 +0200

    feat: 🎸 add env gitignore etc

--- src/App/index.js
+++ src/App/index.js
@@ -8,9 +8,11 @@ import { useQuery } from "react-query";
 import { theme } from "./theme";
 import Header from "../Header";
 
-const fetchPizzas = () =>
-  fetch("http://localhost:3001/pizzas").then((response) => response.json());
-
+const fetchPizzas = () => {
+  const baseUrlApi =
+    process.env.REACT_APP_BASE_URL_API || "http://localhost:3001";
+  return fetch(`${baseUrlApi}/pizzas`).then((response) => response.json());
+};
 export default function App() {
   /*  const [pizzas, setPizzas] = React.useState([]); */
   const { status, data } = useQuery("pizzas", fetchPizzas);
